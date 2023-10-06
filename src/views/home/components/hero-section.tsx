21fe81a309aa8244b70520f24f7ef0ce1f5843e9
RKOrtega94 <rkortega1994@gmail.com>
2023-10-06 17:32:47 -0500

    Update

--- src/views/home/components/hero-section.tsx
+++ src/views/home/components/hero-section.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
   return (
     <div className="w-full flex justify-center bg-black mt-0 p-0 relative aspect-square md:aspect-video">
       <HeroVideo />
-      <div className="absolute bottom-0">
+      <div className="absolute -bottom-20 px-10">
         <HeroCard />
       </div>
     </div>
