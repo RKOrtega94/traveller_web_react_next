b213c77f18693f882fd160c90d83905769c2baad
RKOrtega94 <rkortega1994@gmail.com>
2023-11-07 09:42:14 -0500

    Delete unused code related to sliders and refactor category list view model

--- src/presentation/views_model/category/list-categories.tsx
+++ src/presentation/views_model/category/list-categories.tsx
@@ -1,1 +1,16 @@
-export default async function ListCategoriesViewModel() {}
+import { GetAllCategoriesUseCase } from "@/data/use_cases/category";
+import { CategoryDataSource } from "@data/data_sources";
+
+async function getCategories() {
+  const useCase = new GetAllCategoriesUseCase(new CategoryDataSource());
+  const categories = await useCase.execute();
+  return categories;
+}
+
+export default async function ListCategoriesViewModel() {
+  const categories = await getCategories();
+
+  return {
+    categories,
+  };
+}
