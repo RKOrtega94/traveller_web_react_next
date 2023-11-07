cac8b933a232431344c2c1589a5245045dcb251b
RKOrtega94 <rkortega1994@gmail.com>
2023-11-07 09:46:45 -0500

    Remove CategoriesSlider component from HeroComponent

--- src/presentation/components/header/hero-component.tsx
+++ src/presentation/components/header/hero-component.tsx
@@ -1,14 +1,11 @@
 import ecIcon from "@assets/icons/ec.svg";
 import Image from "next/image";
-import CategoriesSlider from "../sliders/categories-slider";
 
 export default function HeroComponent() {
   return (
     <div className="w-full relative">
       <Image className="w-full" src={ecIcon} alt="ec" objectFit="contain" />
-      <div className="absolute bottom-10">
-        <CategoriesSlider />
-      </div>
+      <div className="absolute bottom-10"></div>
     </div>
   );
 }
