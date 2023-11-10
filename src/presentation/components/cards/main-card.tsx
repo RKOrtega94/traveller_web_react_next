02bb6284ca624fc75bb0c5c904cf364a791fdf77
RKOrtega94 <rkortega1994@gmail.com>
2023-11-10 14:22:28 -0500

    Add category carousel to home page and update main card styles

--- src/presentation/components/cards/main-card.tsx
+++ src/presentation/components/cards/main-card.tsx
@@ -1,6 +1,6 @@
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
-    <div className="cursor-pointer rounded-lg bg-white shadow-md shadow-black border border-black border-b-2 border-r-2 hover:border-b-4 hover:border-r-4">
+    <div className="cursor-pointer rounded-lg bg-white shadow-md shadow-black border border-black border-b-2 border-r-2 hover:border-b-4 hover:border-r-4 my-2">
       {children}
     </div>
   );
--- src/presentation/views/home/components/hero-categories.tsx
+++ src/presentation/views/home/components/hero-categories.tsx
@@ -1,21 +1,44 @@
 "use client";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 import { Category } from "@/domain/entities";
+import { useEffect, useState } from "react";
+import { GetAllCategoriesUseCase } from "@/data/use_cases/category";
+import { CategoryDataSource } from "@/data/data_sources";
+import CategoryCard from "@/presentation/components/cards/category-card";
 
 export default function HeroCategoriesSection() {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const getCategories = new GetAllCategoriesUseCase(new CategoryDataSource());
+
+  useEffect(() => {
+    getCategories.execute().then((data) => setCategories(data));
+    console.log(categories);
+  }, []);
+
   const options = {
     type: "loop",
     gap: "1rem",
     autoplay: true,
-    interval: 500,
+    interval: 1000,
     pauseOnHover: true,
     resetProgress: false,
     arrows: false,
     fixedWidth: "10rem",
+    pagination: false,
+    padding: "0.5rem",
   };
   return (
-    <div className="absolute bottom-0 left-0 w-full bg-black">
-      <div className="carousel-track">Contenido</div>
-    </div>
+    <Splide aria-label="My Favorite Images" options={options}>
+      {/* <SplideSlide>content 1</SplideSlide>
+      <SplideSlide>content 2</SplideSlide> */}
+      {categories.map((category) => (
+        <SplideSlide key={category.id}>
+          <CategoryCard category={category} />
+        </SplideSlide>
+      ))}
+    </Splide>
   );
 }
