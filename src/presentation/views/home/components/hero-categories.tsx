a0ef4c30c49d866cf339f7761fe4866d355a7c1d
Robinson Ortega <rkortega1994@gmail.com>
2023-11-11 14:09:48 +0100

    Add SplideTrack component to hero categories section.

--- src/presentation/views/home/components/hero-categories.tsx
+++ src/presentation/views/home/components/hero-categories.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Splide, SplideSlide, SplideTrack } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 
 import { Category } from "@/domain/entities";
@@ -18,42 +18,36 @@ export default function HeroCategoriesSection() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const next = document.querySelector(
-        ".splide__pagination__page--next"
-      ) as HTMLElement;
-      next?.click();
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [categories]);
-
   const options = {
     type: "loop",
     gap: "1rem",
     autoplay: true,
-    interval: 1000,
+    interval: 3000,
     pauseOnHover: true,
     resetProgress: false,
     arrows: false,
     fixedWidth: "10rem",
     pagination: false,
     padding: "0.5rem",
+    transition: "ease-in-out 0.5s",
+    waitForTransition: false,
   };
   return (
     <Splide
-      aria-label="My Favorite Images"
+      aria-label="Categories"
       options={options}
+      hasTrack={false}
       className="absolute bottom-full z-10 w-full"
     >
-      {categories.map((category) => (
-        <SplideSlide key={category.id}>
-          <CategoryCard category={category} />
-        </SplideSlide>
-      ))}
+      <div className="relative">
+        <SplideTrack>
+          {categories.map((category) => (
+            <SplideSlide key={category.id}>
+              <CategoryCard category={category} />
+            </SplideSlide>
+          ))}
+        </SplideTrack>
+      </div>
     </Splide>
   );
 }
